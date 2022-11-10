0c4151a94f3dbd118117708c8cc286e76d491dba
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-10 17:19:13 +0200

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='projects',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='punk',
+    author_email='',
+    description=''
+)
