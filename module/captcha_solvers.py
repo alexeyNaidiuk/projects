04a83f59bac6d3cf18df303ab0b52832e701e880
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-16 18:28:07 +0200

    commit

--- module/captcha_solvers.py
+++ module/captcha_solvers.py
@@ -3,6 +3,26 @@ import requests
 from config import CAPMONSTER_HOST
 
 
-class CapMonsterCaptchaSolver:
+class Solver:
 
-    ...
+    def solve(self, *args, **kwargs) -> str:
+        ...
+
+
+class CapMonsterSolver(Solver):
+    __url = CAPMONSTER_HOST
+
+    def solve(self, sitekey: str, url: str, tine_for_solving: int = 20) -> str:
+        ...
+
+
+class CapMonsterRecaptcha2Solver(CapMonsterSolver):
+
+    def solve(self, sitekey: str, url: str, tine_for_solving: int = 20) -> str:
+        ...
+
+
+class CapMonsterRecaptcha3Solver(CapMonsterSolver):
+
+    def solve(self, sitekey: str, url: str, tine_for_solving: int = 20) -> str:
+        ...
