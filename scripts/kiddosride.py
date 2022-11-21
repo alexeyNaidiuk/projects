fad81e5e9d6645285e1602e7fb80114492da9088
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-21 11:53:12 +0200

    commit

--- scripts/kiddosride.py
+++ scripts/kiddosride.py
@@ -27,5 +27,5 @@ if __name__ == '__main__':
     promo_link = 'bit.ly/3WQF7J7'
     spam = ConcreteSpam(promo_link, project_name, success_message)
     res = spam.send_post()
-    # if res:
-    #     spam.run_concurrently(5)
+    if res:
+        spam.run_concurrently()
