bfcbe9a7482ce20d368df5757bf8fa72a760ee9e
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-22 11:51:16 +0200

    commit

--- module/project_controller.py
+++ module/project_controller.py
@@ -13,24 +13,28 @@ class ProjectController:
         self.prom_link = prom_link
 
     def send_count(self, count) -> int:
+        '''https://zennotasks.com/automation/api.php?key=FOO&count=1&project=BAR'''
 
         params = {'key': self.__key, 'project': self.project_name, 'count': count}
         response = requests.get(self.__url, params=params)
         return response.status_code
 
     def send_good_status(self) -> int:
+        '''https://zennotasks.com/automation/api.php?key=&status=0&project='''
 
         params = {'key': self.__key, 'status': 0, 'project': self.project_name}
         resp = requests.get(self.__url, params=params)
         return resp.status_code
 
     def send_bad_status(self) -> int:
+        '''https://zennotasks.com/automation/api.php?key=&status=1&project='''
 
         params = {'key': self.__key, 'status': 1, 'project': self.project_name}
         resp = requests.get(self.__url, params=params)
         return resp.status_code
 
     def retrieve_attached_link(self):
+        '''https://zennotasks.com/automation/api.php?key=FOO&getlink=1&project=BAR'''
 
         params = {'key': self.__key, 'getlink': '1', 'project': self.project_name}
         resp = requests.get(self.__url, params=params)
@@ -38,6 +42,7 @@ class ProjectController:
         return content
 
     def status(self) -> bool:
+        '''https://zennotasks.com/automation/api.php?key=FOO&iswork=1&project=BAR&prom_link=BIT.LY/FOOBAR'''
 
         params = {'key': self.__key, 'iswork': '1', 'project': self.project_name, 'prom_link': self.prom_link}
         resp = requests.get(self.__url, params=params)
---
+++ scripts/laveleholidays.py
@@ -0,0 +1,43 @@
+import requests
+
+from module.spam_abstraction import Spam
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Origin': 'http://www.laveleholidays.com',
+    'Referer': 'http://www.laveleholidays.com/booking-form/',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+}
+
+
+class ConcreteSpam(Spam):
+
+    def post(self, target) -> requests.Response:
+        data = {
+            'fullName': self.get_text(),
+            'Telephone': self.get_text(),
+            'Email': target,
+            'CheckInDate': '1111-11-11',
+            'CheckOutDate': '1111-11-11',
+            'NoOfGuests': '1',
+            'sendCopy': '1',
+            'bookingform': 'Submit',
+        }
+
+        response = requests.post('http://www.laveleholidays.com/email_send.php', headers=headers, data=data,
+                                 proxies=self.get_proxies())
+        return response
+
+
+if __name__ == '__main__':
+    success_message = 'Your booking request has been sent '
+    project_name = 'laveleholidays'
+    promo_link = 'bit.ly/3TPCPaM'
+    spam = ConcreteSpam(promo_link, project_name, success_message)
+    res = spam.send_post('worksoftum@gmail.com')
+    if res:
+        spam.run_concurrently(5)
