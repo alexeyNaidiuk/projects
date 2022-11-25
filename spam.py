38879e0cd133a6543f46b60c3f54db08b6d43113
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-25 15:25:41 +0200

    commit

--- spam.py
+++ spam.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     success_message = ''
     project_name = 'project_name'
     promo_link = 'bit.ly/3CEv81r'
-    spam = ConcreteSpam(promo_link, project_name, success_message, logging_level='debug')
+    spam = ConcreteSpam(promo_link, project_name, success_message)
     res = spam.send_post()
     # if res:
     #     spam.run_concurrently()
