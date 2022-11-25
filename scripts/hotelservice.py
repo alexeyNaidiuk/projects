7e0276f0ff7e321a3699bf5e214efc40802d4db2
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-25 11:37:22 +0200

    commit

--- scripts/hotelservice.py
+++ scripts/hotelservice.py
@@ -36,5 +36,5 @@ if __name__ == '__main__':
     promo_link = 'bit.ly/3gs501C'
     spam = ConcreteSpam(promo_link, project_name, success_message)
     res = spam.send_post()
-    # if res:
-    #     spam.run_concurrently()
+    if res:
+        spam.run_concurrently()
