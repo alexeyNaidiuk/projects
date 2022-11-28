d757ee83e76cab603fe313d694c42d4472a325fb
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-28 12:23:15 +0200

    commit

--- scripts/communitymarkets.py
+++ scripts/communitymarkets.py
@@ -26,6 +26,5 @@ if __name__ == '__main__':
     promo_link = 'bit.ly/3EQ3yQ1'
     spam = ConcreteSpam(promo_link, project_name, success_message)
     res = spam.send_post()
-    # if res:
-    #     spam.run_concurrently()
-
+    if res:
+        spam.run_concurrently()
