d7f518d8f62cd677c8a8ec2f7bf0e1942924a2b9
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-30 15:24:28 +0200

    commit

--- scripts/peopleconnect.py
+++ scripts/peopleconnect.py
@@ -11,18 +11,19 @@ url = 'http://www.peopleconnect.com.au/refer-a-friend'
 class ConcreteSpam(module.Spam):
 
     def post(self, target) -> requests.Response:
+        text = self.get_text()
         data = {
             '__EVENTTARGET': '',
             '__EVENTARGUMENT': '',
             '__VIEWSTATE': '/wEPDwUKMjExNDUyMzEwMA9kFgJmD2QWAgIDD2QWAgILD2QWAmYPZBYCAgUPDxYEHghDc3NDbGFzcwUHY3VycmVudB4EXyFTQgICZGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFHmN0bDAwJE1haW5Db250ZW50JEltYWdlQnV0dG9uMegtU4axxtT5deIf5LyONFg3USlC4HaUjr0q8KQiVbUp',
             '__VIEWSTATEGENERATOR': 'CB80D24E',
             '__EVENTVALIDATION': '/wEdAAiH2jUBzOMbBRxUYYe3BFB5XDTsqOiWfjOuHDcHFU+PI3V8fJKCYdhkVJdO+DIfQ0XRvDs5OXQ11rod7fgapnny0xahiDZxRyh5Q2CFuD1fpcm4Dg4her8nMjNi/Z4apy3HZuu2POyOjiZ3O+PdOLc3wcycRn+m1LKiok37o6zL/kLiEtm5syH0VNyxpRKwpHJ0XV89MQrEQIwiZFStbjPM',
-            'ctl00$MainContent$txtYourName': self.get_text(),
+            'ctl00$MainContent$txtYourName': text,
             'ctl00$MainContent$txtYourEmail': target,
-            'ctl00$MainContent$txtName': self.get_text(),
+            'ctl00$MainContent$txtName': text,
             'ctl00$MainContent$txtEmail': target,
-            'ctl00$MainContent$txtPhone': self.get_text(),
-            'ctl00$MainContent$txtComments': self.get_text(),
+            'ctl00$MainContent$txtPhone': text,
+            'ctl00$MainContent$txtComments': text,
             'ctl00$MainContent$ImageButton1.x': '57',
             'ctl00$MainContent$ImageButton1.y': '13',
         }
