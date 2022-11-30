3a2fa1a674a804da386462c2abfad3c20027477c
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-30 14:12:51 +0200

    commit

--- spam.py
+++ spam.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     promo_link = 'bit.ly/3VyF5El'
     spam = ConcreteSpam(project_name, success_message, referal_project_name=project, promo_link=promo_link)
     res = spam.send_post()
-    if res:
-        spam.run_concurrently()
+    # if res:
+    #     spam.run_concurrently()
