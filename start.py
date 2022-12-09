20913982ca4a0a6c479fcdfa12e3f8391ac491c7
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-09 14:45:20 +0200

    commit

--- start.py
+++ start.py
@@ -7,10 +7,6 @@ def run_file(file_name):
 
 
 if __name__ == '__main__':
-    processes = []
     for file in os.listdir('./scripts'):
         proc = Process(target=run_file, args=(file,))
         proc.start()
-        processes.append(proc)
-    for proc in processes:
-        proc.join()
