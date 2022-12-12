4f30c3f2f7828a4ac598b8e44f2bd459acc65cda
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-12 11:43:50 +0200

    commit

--- start.py
+++ start.py
@@ -4,11 +4,11 @@ from multiprocessing import Process
 
 
 def run_file(file_name):
-    os.system(f'python ./archive/{file_name}')
+    os.system(f'python ./scripts/{file_name}')
 
 
 if __name__ == '__main__':
-    for file in os.listdir('./archive'):
+    for file in os.listdir('./scripts'):
         proc = Process(target=run_file, args=(file,))
         proc.start()
-        time.sleep(.7)
+        time.sleep(.2)
