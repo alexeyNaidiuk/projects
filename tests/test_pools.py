ad9ae18df5a975aeb61e934d7afa1e180166ca9e
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-12 11:03:45 +0200

    commit

--- tests/test_pools.py
+++ tests/test_pools.py
@@ -6,16 +6,17 @@ from module.pools import TargetServerPool, ProxyServerPool
 class TestTargetPools(unittest.TestCase):
 
     def test_target_pool(self):
-        pools = {'turkey', 'alotof', 'dbru', 'mixru', 'rub36'}
+        pools = ('turkey', 'alotof', 'dbru', 'mixru', 'rub36', 'dadru')
 
         for pool in pools:
             target_pool = TargetServerPool(pool)
 
             target = target_pool.pop()
+            print(target, pool)
             self.assertTrue('@' in target or '.' in target)
 
     def test_proxy_pool(self):
-        pools = ['wwmix', 'west', 'parsed', 'vlad']
+        pools = ('wwmix', 'west', 'parsed', 'vlad')
 
         for pool in pools:
             proxy_pool = ProxyServerPool(pool)
