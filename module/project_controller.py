801edb912da29f1c7a3335a6f95af8073143ef2c
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-20 14:02:18 +0200

    refactored project controller with cache

--- module/project_controller.py
+++ module/project_controller.py
@@ -39,13 +39,11 @@ class ProjectServerController(ProjectController):
     __key = ZENNO_KEY
 
     def send_count(self, count) -> int:
-
         params = {'key': self.__key, 'project': self.name, 'count': count}
         response = requests.get(self.__url, params=params)
         return response.status_code
 
     def retrieve_attached_link(self) -> str | None:
-
         params = {'key': self.__key, 'getlink': '1', 'project': self.name}
         resp = requests.get(self.__url, params=params)
         content = resp.content.decode()
@@ -90,20 +88,30 @@ class ProjectServerControllerCached(ProjectServerController):
         super().__init__(name, prom_link, project_name, targets_base)
         self.__slots__.append('cached_status_file')
         self.cached_status_file = pathlib.Path(tempfile.mktemp(suffix='.json', prefix=self.name))
+        self._dump_status()
+
+    def _dump_status(self, status: bool | None = None, timestamp: str | datetime.datetime = datetime.datetime.now()):
+        _dump_json(self.cached_status_file, {'status': status, 'timestamp': timestamp})
+
+    def _load_status(self):
+        return _load_json(self.cached_status_file)
+
+    def _check_status(self, timestamp) -> bool:
+        timestamp = datetime.datetime.fromisoformat(timestamp)
+        check_status = datetime.datetime.now() > timestamp + datetime.timedelta(seconds=STATUS_EXPIRATION_LIMIT_IN_SEC)
+        return check_status
 
     def get_status(self) -> bool:
         if not self.name:
             return False
-        if self.cached_status_file.exists():
-            cached_status_dict: dict = _load_json(self.cached_status_file)
-            timestamp = datetime.datetime.fromisoformat(cached_status_dict['timestamp'])
-            status: bool = cached_status_dict['status']
+        cached_status_dict: dict = self._load_status()
+        status: bool = cached_status_dict['status']
+        timestamp = cached_status_dict['timestamp']
+        expired: bool = self._check_status(timestamp)
 
-            if datetime.datetime.now() > timestamp + datetime.timedelta(seconds=STATUS_EXPIRATION_LIMIT_IN_SEC):
-                status: bool = super().get_status()
-                _dump_json(self.cached_status_file, {'status': status, 'timestamp': datetime.datetime.now()})
-        else:
+        if status is None or expired:
             status: bool = super().get_status()
-            _dump_json(self.cached_status_file, {'status': status, 'timestamp': datetime.datetime.now()})
-
-        return status
+            self._dump_status(status)
+            return status
+        else:
+            return status
