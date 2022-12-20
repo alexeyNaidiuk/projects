f0dac68577288351b563705a843f22282dc7a2a1
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-20 15:20:08 +0200

    link shortner is now depends on server

--- module/link_shortner.py
+++ module/link_shortner.py
@@ -1,32 +1,21 @@
-import logging
-
 import requests
 
-from module.config import ZENNO_KEY
+from module.config import SERV_HOST
 
 
-class LinkShortner:
-    __url = 'https://zennotasks.com/automation/api.php'
-    __key = ZENNO_KEY
-    __referals = {
-        'supercat': 'https://referencemen.live/ktVmDV?c=0098xLek_pT9MBd059d7cb95430c53',
-        'luckybird': 'https://referencemen.live/ktVmDV?c=0097xLek_pT9MB5b4d7a1cbb052b1b',
-        'allright': 'https://referencemen.live/ktVmDV?c=0114xLek_pT9MBc578b8a2b8d59856',
-        'fortuneclock': 'https://referencemen.live/ktVmDV?c=0133xLek_pT9MB5ea167c052b0c66a'
-    }
+class Shortner:
 
     @classmethod
     def get_link(cls, target_pool_name: str, referal_to_project: str) -> str:
-        project_link = cls.__referals[referal_to_project]
+        ...
 
-        if not referal_to_project:
-            utm_campaign = target_pool_name
-        else:
-            utm_campaign = f'{target_pool_name}_{referal_to_project}'
 
-        url = f'{project_link}&utm_campaign={utm_campaign}'
-        logging.debug(url)
-        params = {'key': cls.__key, 'shurl': url}
-        response = requests.get(cls.__url, params=params)
-        content = response.text
-        return content
+class LinkShortner(Shortner):
+
+    @classmethod
+    def get_link(cls, target_pool_name: str, referal_to_project: str) -> str:
+        link = requests.get(
+            f'http://{SERV_HOST}/link',
+            params={'targets_base': target_pool_name, 'project_name': referal_to_project}
+        ).text
+        return link
--- tests/test_link_shortner.py
+++ tests/test_link_shortner.py
@@ -1,20 +1,17 @@
-import requests
-
 from unittest import TestCase
+
+import requests
 
 from module.link_shortner import LinkShortner
 
 
 class TestLinkShortner(TestCase):
 
-    def test_get_link(self):
+    def test_link(self):
         target_pool_name = 'mixru'
         referal_to_project = 'luckybird'
 
         shortened_link = LinkShortner.get_link(target_pool_name=target_pool_name, referal_to_project=referal_to_project)
-
-        self.assertIn('bit.ly', shortened_link)
         response = requests.get('https://%s' % shortened_link)
-
         response_content = response.text
         self.assertIn(referal_to_project, response_content)
