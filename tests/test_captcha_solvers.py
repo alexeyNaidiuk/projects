e38ba0442fe5ca0d9cf661696b2a2fbefe5eb1d8
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-12-20 13:14:14 +0200

    bug fix

--- tests/test_captcha_solvers.py
+++ tests/test_captcha_solvers.py
@@ -1,15 +1,10 @@
-from unittest import TestCase
-
-from module.captcha_solvers import CapMonsterRecaptcha2Solver
-
-
-class TestCapMonsterRecaptcha2solver(TestCase):
-
-    def test_solving(self):
-        solver = CapMonsterRecaptcha2Solver()
-
-        googlesitekey = ' 6LfCnhwTAAAAAONEzvCPHkac-biXc_1zUPqxsrX3'
-        pageurl = 'https://mountainexcursion.is/contact-us.html'
-        result = solver.solve(googlekey=googlesitekey, pageurl=pageurl)
-
-        self.assertNotEqual(result, None)
+# class TestCapMonsterRecaptcha2solver(TestCase):
+#
+#     def test_solving(self):
+#         solver = CapMonsterRecaptcha2Solver()
+#
+#         googlesitekey = ' 6LfCnhwTAAAAAONEzvCPHkac-biXc_1zUPqxsrX3'
+#         pageurl = 'https://mountainexcursion.is/contact-us.html'
+#         result = solver.solve(googlekey=googlesitekey, pageurl=pageurl)
+#
+#         self.assertNotEqual(result, None)
--- tests/test_pools.py
+++ tests/test_pools.py
@@ -1,3 +1,4 @@
+import logging
 import unittest
 
 from module.pools import TargetServerPool, ProxyServerPool
@@ -6,13 +7,13 @@ from module.pools import TargetServerPool, ProxyServerPool
 class TestTargetPools(unittest.TestCase):
 
     def test_target_pool(self):
-        pools = ('turkey', 'alotof', 'dbru', 'mixru', 'rub36', 'dadru')
+        pools = ('turk', 'alotof', 'dbru', 'mixru', 'rub36', 'dadru')
 
         for pool in pools:
             target_pool = TargetServerPool(pool)
-
             target = target_pool.pop()
-            print(target, pool)
+            logging.debug(pool)
+            logging.debug(target)
             self.assertTrue('@' in target or '.' in target)
 
     def test_proxy_pool(self):
